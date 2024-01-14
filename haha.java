04291972a0ceb07b742ef5f9359f1736bacbe2c3
Danindunadith <117801278+Danindunadith@users.noreply.github.com>
2024-01-14 22:52:33 +0530

    Add files via upload

---
+++ haha.java
@@ -0,0 +1,15 @@
+package skill_lab;
+
+public class haha {
+
+	static  void myMethod() {
+		System.out.println("executed");
+	}
+	
+	
+	
+	public static void main (String [] args)
+	{
+		myMethod();
+	}
+}
---
+++ newwe.java
@@ -0,0 +1,10 @@
+package skill_lab;
+
+public class newwe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
